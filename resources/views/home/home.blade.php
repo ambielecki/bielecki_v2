ec2080f71bdbf1b1246849aee453b0ffff9397f4
Andrew Bielecki <andrew.bielecki@healthcaresource.com>
2020-02-20 13:15:27 -0500

    Good Enough

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -8,15 +8,40 @@
                     <div class="card">
                         <div class="card-content">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et ultricies metus. Suspendisse lectus nibh, finibus vel malesuada et, ornare a diam. Aenean et risus vitae lacus tempor eleifend at eget tellus. Nunc pulvinar elit enim, eu porta turpis vulputate sed. Suspendisse massa sem, viverra vitae lacinia eu, pharetra sed neque. Donec eu risus vel mi auctor fermentum vitae quis augue. Etiam convallis, enim a porta auctor, metus dolor consequat odio, ut lacinia urna lorem nec arcu.
-
-                                Morbi varius lobortis mi eget porttitor. Morbi suscipit feugiat nulla vel faucibus. Maecenas eget est gravida, molestie magna sed, finibus orci. Donec feugiat ornare lorem, vitae fringilla purus bibendum at. Nulla vehicula, tellus vel elementum hendrerit, ante libero pretium quam, et cursus ante nulla quis lectus. Sed suscipit convallis arcu, sit amet efficitur nibh elementum non. Suspendisse vitae ex risus. Aliquam erat volutpat. Sed quam metus, maximus ac dictum tristique, facilisis ac eros. Suspendisse sollicitudin tortor ac mi sagittis egestas. Sed interdum massa sed nisl vulputate convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque imperdiet, risus at auctor efficitur, mauris enim malesuada nulla, ac ornare purus diam sit amet ligula. Sed posuere malesuada nunc in imperdiet. Vestibulum gravida, leo in commodo pellentesque, ipsum ipsum vestibulum diam, at vulputate risus nibh ac mauris. Duis euismod neque mauris, et tincidunt ligula tristique et.
-
-                                Quisque maximus efficitur turpis vitae bibendum. Etiam sapien quam, tincidunt interdum volutpat a, dictum quis purus. Fusce dui nulla, rhoncus et convallis tincidunt, porttitor ac tellus. Fusce commodo urna augue, vel viverra ex eleifend non. Maecenas volutpat varius efficitur. Etiam tortor diam, feugiat in tempus vitae, blandit non nisl. Etiam eros sem, consectetur sit amet ex in, sodales congue dui. Aliquam ac odio tellus.
-
-                                Duis sit amet lacinia ligula. Aliquam sem est, molestie eget dapibus ac, bibendum et risus. Nulla convallis aliquam erat vel molestie. Proin faucibus eget turpis vel pellentesque. Nulla facilisi. Vestibulum lobortis viverra tellus in rutrum. Morbi pretium placerat mauris, non varius ipsum volutpat gravida. Ut pharetra ullamcorper eleifend. Quisque ex libero, tincidunt et feugiat nec, imperdiet faucibus elit. Quisque augue erat, hendrerit sed efficitur ac, semper eleifend felis.
-
-                                Aenean fringilla mattis leo, in tristique nisi pulvinar at. Suspendisse pretium nec orci sed viverra. Ut lobortis nisl ac risus ultrices vestibulum. Nam vehicula sodales nunc, eget fringilla arcu commodo ac. Etiam vitae mattis sem. Integer lobortis orci at mauris laoreet varius. Praesent quis pharetra libero, vitae consectetur dolor. Sed lobortis, ante vel bibendum malesuada, urna libero pellentesque dui, feugiat placerat nibh dui ac velit. Sed vulputate ante vitae egestas fringilla. Nullam tortor enim, gravida id hendrerit vel, dictum at nulla.
+                                Hello, welcome to my homepage.  I am Andrew Bielecki, a software
+                                engineer living just north of Boston, MA.  I am still relatively
+                                new to the field (about 4 years now) as I spent over a decade after
+                                university working as a bench scientist in the biopharmaceutical industry.
+                                I will be finishing up my Master's degree in Information Management
+                                Systems from Harvard Extension School in the Spring of 2020.
+                            </p>
+                            <br>
+                            <p>
+                                My primary focus has been working with PHP on the backend side of things,
+                                but I have experience in frontend systems as well.  I've worked with
+                                Laravel, Yii, Symfony, and a number of homegrown systems in PHP for both web
+                                backends and general ETL scripting. Most of my frontend work has been with traditional
+                                HTML / CSS, incorporating CSS frameworks such as Bootstrap, but I've also had some opportunity to
+                                get familiar with modern JS frameworks, having worked with both VueJS and Angular. And of
+                                course I've had the need / opportunity to work in other languages such as Python and Java
+                                when it fit our projects or was needed for the task at hand.
+                            </p>
+                            <br>
+                            <p>
+                                I began working in the software industry at <a href="https://www.business.com/" target="_blank">Business.com</a>, starting with
+                                a homegrown PHP framework and later with Symfony and Yii.  From there I moved to
+                                <a href="https://mobilefuse.com/" target="_blank">Mobilefuse</a>, an adtech company,
+                                primarily working with an in house dashboard written in Laravel,
+                                and a second project, also in Laravel, that focused more on ETL / cron scripts.  After
+                                Mobilrfuse I joined the team at <a href="https://www.healthcaresource.com/" target="_blank">Healthcaresource</a>,
+                                where I work on their assessments products which are written in both a homegrown PHP framework and Yii.
+                            </p>
+                            <br>
+                            <p>
+                                In my spare time I enjoy biking, scuba diving, and coaching baseball for
+                                my son's Little League team.  Like everyone I have more half finished
+                                hobby projects than I know what to do with, maybe one day I will have the time
+                                to finish one off.
                             </p>
                         </div>
                     </div>
@@ -29,14 +54,24 @@
                                 <div class="card-content">
                                     <div class="media">
                                         <div class="media-left">
-                                            <figure class="image is-48x48">
-                                                <img class="is-rounded" src="/images/avatar.jpg">
+                                            <figure class="image is-96x96">
+                                                <a href="https://divelog.andrewbielecki.com" target="_blank"><img src="/images/divelogrepeat.png"></a>
                                             </figure>
                                         </div>
 
                                         <div class="media-content">
-                                            <p><a href="https://github.com/ambielecki"><i class="fab fa-github"></i> Find me on Github</a></p>
-                                            <p><a href="https://www.linkedin.com/in/andrew-bielecki-5835b5123/"><i class="fab fa-linkedin"></i> Connect on LinkedIn</a></p>
+                                            <p>
+                                                You can find one of those half finished hobby projects here,
+                                                <a href="https://divelog.andrewbielecki.com" target="_blank">DiveLogRepeat</a>.
+                                                It is supposed to become a online divelog, but I've only finished off
+                                                the dive calculator section, which lets you get some important stats off the
+                                                PADI dive tables.
+                                            </p>
+                                            <p>
+                                                It is written in Laravel with some Vue sprinkled in, running on a typical
+                                                LAMP stack on a small Digital Ocean droplet. You can find the github
+                                                repo here: <a href="https://github.com/ambielecki/divelog_2019" target="_blank">Github Repo</a>.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
