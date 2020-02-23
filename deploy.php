0166346406f117a65a1614abaa4354c9a8005078
Andrew Bielecki <andrewb@mobilefuse.com>
2020-02-23 13:10:27 -0500

    Add deploy file

---
+++ deploy.php
@@ -0,0 +1,46 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'Bielecki');
+
+// Project repository
+set('repository', 'git@github.com:ambielecki/bielecki_v2.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('andrewbielecki.com')
+    ->user('ambielecki')
+    ->stage('production')
+    ->set('deploy_path', '/var/www/html/bielecki');
+
+// Tasks
+
+task('pwd', function () {
+    $result = run('pwd');
+    writeln("Current dir: {$result}");
+});
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+//before('deploy:symlink', 'artisan:migrate');
